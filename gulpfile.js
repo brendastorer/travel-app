78b4bd43dab598cc47e113e85bd9264d20e5a40b
Brenda Storer <brendastorer@gmail.com>
2018-01-07 11:11:30 -0500

    Add autoprefixer to gulp scripts

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,51 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var babel = require('gulp-babel');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function () { 
-    console.log('Hello Gulp!') 
-    gulp.watch( "./assets/scss/**/*.scss", ["styles"] );
+gulp.task('default', () => { 
+  gulp.watch( "./assets/scss/**/*.scss", ["styles"] );
 });
 
-gulp.task('watch', function () { 
-    /** Watch any changes */
-    gulp.watch( "./assets/scss/**/*.scss", ["styles"] );
-    gulp.watch( "./assets/scripts/**/*.js", ["scripts"] );
+gulp.task('watch', () => { 
+  gulp.watch( "./assets/scss/**/*.scss", ["styles"] );
+  gulp.watch( "./assets/scripts/**/*.js", ["scripts"] );
 });
 
-//style paths
-var sassFiles = './assets/scss/**/*.scss',
+const sassFiles = './assets/scss/**/*.scss',
 cssDest = './public/css/';
 
-gulp.task('styles', function(){
-gulp.src(sassFiles)
-  .pipe(sass().on('error', sass.logError))
-  .pipe(gulp.dest(cssDest));
+gulp.task('styles', () => {
+  gulp.src(sassFiles)
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest(cssDest));
 });
 
-//script paths
-var jsFiles = './assets/scripts/**/*.js',
+const jsFiles = './assets/scripts/**/*.js',
 jsDest = './public/javascript';
 
-gulp.task('scripts', function() {
+gulp.task('scripts', () => {
   return gulp.src(jsFiles)
     .pipe(babel({
       presets: [
-          ['es2015', {modules: false}],
+        ['es2015', {modules: false}],
       ],
       plugins: [
-          ['transform-es2015-modules-commonjs', {strictMode: false}],
+        ['transform-es2015-modules-commonjs', {strictMode: false}],
       ]
     }))
     .pipe(concat('app.js'))
     .pipe(gulp.dest(jsDest))
     .pipe(rename('app.min.js'))
-    .pipe(uglify().on('error', function(e){
-            console.log(e);
-         }))
+    .pipe(uglify().on('error', function(err){
+      console.log(err);
+    }))
     .pipe(gulp.dest(jsDest));
 });
